3ab9ca5fd8724058d9b1235a122689a3aff20447
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-05-15 18:43:10 +0900

    Add unit tests for mvp

---
+++ app/src/test/java/PersonTest.java
@@ -0,0 +1,30 @@
+import com.quangnguyen.hellomvp.Person;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Quang Nguyen.
+ */
+
+public class PersonTest {
+
+  @Test
+  public void testCreation() {
+    Person person1 = new Person();
+    assertEquals("First name should be null", person1.getFirstName(), null);
+    assertEquals("Last name should be null", person1.getLastName(), null);
+
+    Person person2 = new Person("first", "last");
+    assertEquals("First name should be 'fist'", person2.getFirstName(), "first");
+    assertEquals("Last name should be 'last'", person2.getLastName(), "last");
+  }
+
+  @Test
+  public void testUpdateName() {
+    Person person = new Person("first", "last");
+    person.setFirstName("FIRST");
+    person.setLastName("LAST");
+  }
+
+}
---
+++ app/src/test/java/SayHelloPresenterTest.java
@@ -0,0 +1,46 @@
+import com.quangnguyen.hellomvp.Person;
+import com.quangnguyen.hellomvp.SayHelloContract;
+import com.quangnguyen.hellomvp.SayHelloPresenter;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+/**
+ * @author Quang Nguyen.
+ */
+
+public class SayHelloPresenterTest {
+
+  public static final String FIRST_NAME = "Quang";
+
+  public static final String LAST_NAME = "Nguyen";
+
+  @Mock
+  private SayHelloContract.View sayHelloView;
+
+  private SayHelloContract.Presenter sayHelloPresenter;
+  @Before
+  public void setup() {
+    // When use @Mock annotation, we need to trigger the creation of annotated objects by calling iniMocks() method.
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void loadMessage() {
+    sayHelloPresenter = new SayHelloPresenter(sayHelloView);
+    sayHelloPresenter.loadMessage();
+    verify(sayHelloView).showError("No person name found."); // Person Object has just created but no data.
+  }
+
+  @Test
+  public void saveName() {
+    sayHelloPresenter = new SayHelloPresenter(sayHelloView);
+    sayHelloPresenter.saveName(FIRST_NAME, LAST_NAME);
+    // Because of stupidly dependent creation of Model in Presenter, we can not test View behavior.
+    // OK, test View instead
+    sayHelloPresenter.loadMessage();
+    verify(sayHelloView).showMessage("Hi " + FIRST_NAME + " " + LAST_NAME + "!");
+  }
+}
