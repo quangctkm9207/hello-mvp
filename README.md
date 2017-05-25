9e04269ec17577590e0d819f0738375953b34fdc
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-05-25 13:23:51 +0900

    Add UI tests

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 Full details are explained in the following Medium posts:
 1. [Archiecture Patterns in Android](https://android.jlelse.eu/architecture-patterns-in-android-abf99f2b6f70).
 2. [Android MVP with Dependency Injection](https://android.jlelse.eu/android-mvp-architecture-with-dependency-injection-dee43fe47af0) (Note: checkout 'dagger' branch).
+3. [Falling in love with Android Testing](https://blog.mindorks.com/falling-in-love-with-android-testing-dd11ffa6ac3e)
--- app/src/test/java/com/quangnguyen/hellomvp/SayHelloPresenterTest.java
+++ app/src/test/java/com/quangnguyen/hellomvp/SayHelloPresenterTest.java
@@ -25,7 +25,7 @@ public class SayHelloPresenterTest {
   private SayHelloContract.Presenter sayHelloPresenter;
   @Before
   public void setup() {
-    // When use @Mock annotation, we need to trigger the creation of annotated objects by calling iniMocks() method.
+    // When use @Mock annotation, we need to trigger the creation of annotated objects by calling initMocks() method.
     MockitoAnnotations.initMocks(this);
   }
 
@@ -40,7 +40,7 @@ public class SayHelloPresenterTest {
   public void saveName() {
     sayHelloPresenter = new SayHelloPresenter(sayHelloView);
     sayHelloPresenter.saveName(FIRST_NAME, LAST_NAME);
-    // Because of stupidly dependent creation of Model in Presenter, we can not test View behavior.
+    // Because of stupidly dependent creation of Model in Presenter, we can not test Model behavior.
     // OK, test View instead
     sayHelloPresenter.loadMessage();
     verify(sayHelloView).showMessage("Hi " + FIRST_NAME + " " + LAST_NAME + "!");
